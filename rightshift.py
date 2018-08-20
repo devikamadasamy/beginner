a11659e9e6b0ac70b6d2e026fc7ca2884d17027e
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-08-20 22:51:48 +0530

    Update rightshift.py

--- rightshift.py
+++ rightshift.py
@@ -1,10 +1,14 @@
-# your code goes here
 n=input()
 list1=n.split()
 string=input()
 list2=string.split()
-n=int(list2[1])
-m=int(list2[0])
-list2=list2[m-n-1:]+list2[:m-n-1]
+n=int(list1[1])
+m=int(list1[0])
+if m<n:
+	while m>n:
+		n=n-m
+
+if(m!=1):
+	list2=list2[m-n:]+list2[:m-n]
 k=" ".join(list2)
 print(k)
